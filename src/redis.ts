2d773d4b10df057ce18cb2ef59c4add3d9f197f7
Robert Molina <rmolinamir@gmail.com>
2020-02-09 21:48:04 -0500

    redis comments

--- src/redis.ts
+++ src/redis.ts
@@ -1,11 +1,11 @@
 // Libraries
 import Redis from "ioredis";
 
-// TODO: Add redis URL to .env variables
+// TODO: Add redis settings to the ENV variables file.
 // Connect to 127.0.0.1:6379, db "redis", using password "authpassword":
 // Ex: "redis://:authpassword@127.0.0.1:6379/redis"
-// NOTE: For this to work, make sure request credentials is set up correctly
-// in the playground: "request.credentials": "include"
+// NOTE: For this to work, make sure the request credentials setting is
+// set up correctly in the playground: "request.credentials": "include".
 export const redis = new Redis({
   port: 6379, // Redis port
   host: 'localhost', // Redis host - TODO: Add to .env
