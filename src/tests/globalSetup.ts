9cdaf20a80e8d7b9a70fa2467f219519e2563624
Robert Molina <rmolinamir@gmail.com>
2020-02-16 20:37:30 -0500

    fixed tests global setup

--- src/tests/globalSetup.ts
+++ src/tests/globalSetup.ts
@@ -2,6 +2,11 @@
 import 'module-alias/register';
 import 'reflect-metadata';
 
+// Setting up our configuration
+import { setupConfig } from '../config';
+
+setupConfig(setupConfig.ENodeEnvConfig.TEST);
+
 export default async (): Promise<void> => {
   console.log('Starting tests, and setting up module aliases.');
 };
